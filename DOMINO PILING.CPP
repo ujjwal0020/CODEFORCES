c7d0708b66f02baa2dbe2382a51946861f36016b
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-05 23:16:29 +0530

    Create DOMINO PILING.CPP

---
+++ DOMINO PILING.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    if(m&&n>=1 && m&&n<=16){
+    int s;
+    s=m*n;
+    cout<<s/2<<endl;
+}
+    return 0;
+}
